cec68f3391f4a8e7fb251bcec6baaa8401663f7d
Huyentsuk0176 <Phthihuyenta03@gmail.com>
2026-03-16 15:21:15 +0900

    module4_session8_ex3_uploadavatar_luutrufilecoban

---
+++ src/main/java/com/example/employee_api/controller/FileController.java
@@ -0,0 +1,34 @@
+package com.example.employee_api.controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/api/files")
+public class FileController {
+
+    @PostMapping("/upload")
+    public String uploadFile(@RequestParam("image") MultipartFile file) throws IOException {
+        if (file.isEmpty()){
+            return "File rong";
+        }
+
+        String uploadDir = System.getProperty("user.dir")+"uploads/";
+
+        File dir = new File(uploadDir);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+
+        String fileName = file.getOriginalFilename();
+
+        File dest = new File(uploadDir+fileName);
+
+        file.transferTo(dest);
+
+        return "Upload thành công: " + fileName;
+    }
+}
